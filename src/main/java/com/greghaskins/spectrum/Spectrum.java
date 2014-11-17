0d2fbdcc1cd7cd84f8a1a41e3a49039f0d9f3583
Gregory Haskins <greg@greghaskins.com>
2014-11-17 05:34:18 -0500

    start over to experiment with functional API

--- src/main/java/com/greghaskins/spectrum/Spectrum.java
+++ src/main/java/com/greghaskins/spectrum/Spectrum.java
@@ -1,50 +1,21 @@
 package com.greghaskins.spectrum;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
 
-import com.greghaskins.spectrum.runner.Context;
-
 public class Spectrum extends Runner {
 
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target({ ElementType.TYPE })
-    public static @interface Describe {
-        String value();
-    }
-
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target({ ElementType.METHOD })
-    public static @interface It {
-        String value();
-    }
-
-    @Retention(RetentionPolicy.RUNTIME)
-    @Target({ ElementType.METHOD })
-    public @interface BeforeEach {
-
-    }
-
-    private final Context<?, Void> context;
-
-    public <T> Spectrum(final Class<T> testClass) {
-        context = Context.forClass(testClass);
+    public Spectrum(final Class<?> testClass) {
     }
 
     @Override
     public Description getDescription() {
-        return context.getDescription();
+        return null;
     }
 
     @Override
     public void run(final RunNotifier notifier) {
-        context.run(notifier);
     }
 
 }
