4575c38ba4ba2e2f873386ac51e25010c7550e39
Gregory Haskins <greg@greghaskins.com>
2014-11-23 09:02:45 -0500

    add example spec to demonstrate features

---
+++ src/test/java/specs/ExampleSpec.java
@@ -0,0 +1,50 @@
+package specs;
+
+import static com.greghaskins.spectrum.Spectrum.describe;
+import static com.greghaskins.spectrum.Spectrum.it;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.runner.RunWith;
+
+import com.greghaskins.spectrum.Spectrum;
+import com.greghaskins.spectrum.Spectrum.Block;
+
+@RunWith(Spectrum.class)
+public class ExampleSpec {{
+
+    describe("A spec", () -> {
+
+        final int foo = 1;
+
+        it("is just a code block with a run() method", new Block() {
+            @Override
+            public void run() throws Throwable {
+                assertEquals(1, foo);
+            }
+        });
+
+        it("can also be a lambda function, which is a lot prettier", () -> {
+            assertEquals(1, foo);
+        });
+
+        it("can use any assertion library you like", () -> {
+            org.junit.Assert.assertEquals(1, foo);
+            org.hamcrest.MatcherAssert.assertThat(true, is(true));
+        });
+
+        describe("nested inside a second describe", () -> {
+
+            final int bar = 1;
+
+            it("can reference both scopes as needed", () -> {
+                assertThat(bar, is(equalTo(foo)));
+            });
+
+        });
+
+    });
+
+}}
