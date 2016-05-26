65954ba3b3c69863bd413c1d727c18d38b973860
Gregory Haskins <greg@greghaskins.com>
2016-05-25 23:11:14 -0400

    Make sure tests cover deprecated value(Class<T>) method

--- src/test/java/specs/MutableWrapperSpec.java
+++ src/test/java/specs/MutableWrapperSpec.java
@@ -13,6 +13,7 @@ import com.greghaskins.spectrum.Spectrum.Value;
 import org.junit.runner.RunWith;
 
 @RunWith(Spectrum.class)
+@SuppressWarnings("deprecation")
 public class MutableWrapperSpec {
   {
 
@@ -33,6 +34,11 @@ public class MutableWrapperSpec {
       it("has a default value of null if not specified", () -> {
         final Value<String> name = value();
         assertThat(name.value, is(nullValue()));
+      });
+
+      it("can take an explicit class parameter, for backward compatibility with ~0.6.1", () -> {
+        final Value<String> stringValue = value(String.class);
+        assertThat(stringValue.value, is(nullValue()));
       });
 
     });
