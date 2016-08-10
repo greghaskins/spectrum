32399ac38b2f03ec1f428c0b5591d7054ab053f2
Gregory Haskins <greg@greghaskins.com>
2016-08-10 16:28:47 -0400

    Make it easier for tests to run Spectrum without relfection
    Using SpectrumHelper.run(Block)

--- src/test/java/com/greghaskins/spectrum/SpectrumHelper.java
+++ src/test/java/com/greghaskins/spectrum/SpectrumHelper.java
@@ -1,13 +1,25 @@
 package com.greghaskins.spectrum;
 
+import com.greghaskins.spectrum.Spectrum.Block;
+
+import org.junit.runner.Description;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Request;
 import org.junit.runner.Result;
+import org.junit.runner.Runner;
 
 public class SpectrumHelper {
 
   public static Result run(final Class<?> specClass) throws Exception {
-    return new JUnitCore().run(Request.runner(new Spectrum(specClass)));
+    return runWithJUnit(new Spectrum(specClass));
+  }
+
+  public static Result run(final Block block) {
+    return runWithJUnit(new Spectrum(Description.createSuiteDescription(block.getClass()), block));
+  }
+
+  private static Result runWithJUnit(final Runner runner) {
+    return new JUnitCore().run(Request.runner(runner));
   }
 
 }
