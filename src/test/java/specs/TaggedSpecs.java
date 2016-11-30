293fef50bae3bbff91da84335c4b22c31cbdea3e
Gregory Haskins <greg@greghaskins.com>
2016-11-30 03:17:34 -0500

    Reorganize TaggedSpecs into groups

--- src/test/java/specs/TaggedSpecs.java
+++ src/test/java/specs/TaggedSpecs.java
@@ -28,98 +28,110 @@ public class TaggedSpecs {
     describe("A suite with tagging", () -> {
       beforeEach(TaggedSpecs::clearSystemProperties);
 
-      it("runs completely when no tag selection applied", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsOnly());
-        assertThat(result.getIgnoreCount(), is(0));
-      });
+      describe("configured functionally", () -> {
 
-      it("runs completely when its tag is in the includes list", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsIncluded());
-        assertThat(result.getIgnoreCount(), is(0));
-      });
-
-      it("does not run when it's missing from the includes", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncluded());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("does not run when it's in the excludes list", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsExcluded());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("runs completely when its tag is in the includes list by annotation", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsIncludedByAnnotation());
-        assertThat(result.getIgnoreCount(), is(0));
-      });
-
-      it("does not run when it's missing from the includes by annotation", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncludedByAnnotation());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("does not run when it's in the excludes list by annotation", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsExcludedByAnnotation());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("runs completely when its tag is in the includes list by system property", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsIncludedBySystemProperty());
-        assertThat(result.getIgnoreCount(), is(0));
-      });
-
-      it("does not run when it's missing from the includes by system property", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncludedBySystemProperty());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("does not run when it's in the excludes list by system property", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithTagsExcludedBySystemProperty());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("applies tags recursively to child suites", () -> {
-        final Result result = SpectrumHelper.run(getExcludedSuiteWithNestedSuite());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("is possible for the exclusion tags to be modified part way through the definition",
-          () -> {
-            final Result result = SpectrumHelper.run(getSuiteWhereExclusionIsOverridden());
-            assertThat(result.getIgnoreCount(), is(1));
-          });
-
-      it("is not allowed to be untagged when there's an requiredTags set up", () -> {
-        final Result result =
-            SpectrumHelper.run(getSuiteWithNoTagsThatShouldNotRunBecauseOfIncludeTags());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("is possible to exclude individual specs with tags", () -> {
-        final Result result = SpectrumHelper.run(getSuiteWithOneExcludedTaggedSpec());
-        assertThat(result.getIgnoreCount(), is(1));
-      });
-
-      it("only runs specs that match the requireTags filter", () -> {
-        final ArrayList<String> specsRun = new ArrayList<>();
-
-        SpectrumHelper.run(() -> {
-          requireTags("foo");
-
-          it("should run spec 1", with(tags("foo"), () -> {
-            specsRun.add("spec 1");
-          }));
-          it("should not run spec 2", with(tags("bar"), () -> {
-            specsRun.add("spec 2");
-          }));
-          it("should not run spec 3", () -> {
-            specsRun.add("spec 3");
-          });
-
+        it("runs completely when no tag selection applied", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsOnly());
+          assertThat(result.getIgnoreCount(), is(0));
         });
 
-        assertThat(specsRun, hasSize(1));
-        assertThat(specsRun, contains("spec 1"));
+        it("runs completely when its tag is in the includes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsIncluded());
+          assertThat(result.getIgnoreCount(), is(0));
+        });
+
+        it("does not run when it's missing from the includes", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncluded());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("does not run when it's in the excludes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsExcluded());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("is not allowed to be untagged when there's an requiredTags set up", () -> {
+          final Result result =
+              SpectrumHelper.run(getSuiteWithNoTagsThatShouldNotRunBecauseOfIncludeTags());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("is possible to exclude individual specs with tags", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithOneExcludedTaggedSpec());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("only runs specs that match the requireTags filter", () -> {
+          final ArrayList<String> specsRun = new ArrayList<>();
+
+          SpectrumHelper.run(() -> {
+            requireTags("foo");
+
+            it("should run spec 1", with(tags("foo"), () -> {
+              specsRun.add("spec 1");
+            }));
+            it("should not run spec 2", with(tags("bar"), () -> {
+              specsRun.add("spec 2");
+            }));
+            it("should not run spec 3", () -> {
+              specsRun.add("spec 3");
+            });
+
+          });
+
+          assertThat(specsRun, hasSize(1));
+          assertThat(specsRun, contains("spec 1"));
+        });
+
+      });
+
+      describe("configured by annotation", () -> {
+
+        it("runs completely when its tag is in the includes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsIncludedByAnnotation());
+          assertThat(result.getIgnoreCount(), is(0));
+        });
+
+        it("does not run when it's missing from the includes", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncludedByAnnotation());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("does not run when it's in the excludes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsExcludedByAnnotation());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("applies tags recursively to child suites", () -> {
+          final Result result = SpectrumHelper.run(getExcludedSuiteWithNestedSuite());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("is possible for the exclusion tags to be modified part way through the definition",
+            () -> {
+              final Result result = SpectrumHelper.run(getSuiteWhereExclusionIsOverridden());
+              assertThat(result.getIgnoreCount(), is(1));
+            });
+
+      });
+
+      describe("configured by system property", () -> {
+
+        it("runs completely when its tag is in the includes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsIncludedBySystemProperty());
+          assertThat(result.getIgnoreCount(), is(0));
+        });
+
+        it("does not run when it's missing from the includes", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsNotIncludedBySystemProperty());
+          assertThat(result.getIgnoreCount(), is(1));
+        });
+
+        it("does not run when it's in the excludes list", () -> {
+          final Result result = SpectrumHelper.run(getSuiteWithTagsExcludedBySystemProperty());
+          assertThat(result.getIgnoreCount(), is(1));
+
+        });
       });
 
     });
