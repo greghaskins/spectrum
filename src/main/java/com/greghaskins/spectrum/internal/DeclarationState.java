97f4ad8ebc38f0241e1dfa44f8be81af68a951de
Gregory Haskins <greg@greghaskins.com>
2017-03-11 21:52:56 -0500

    Break cycle btw Specification and DeclarationState

--- src/main/java/com/greghaskins/spectrum/internal/DeclarationState.java
+++ src/main/java/com/greghaskins/spectrum/internal/DeclarationState.java
@@ -1,7 +1,6 @@
 package com.greghaskins.spectrum.internal;
 
 import com.greghaskins.spectrum.Block;
-import com.greghaskins.spectrum.dsl.specification.Specification;
 import com.greghaskins.spectrum.internal.hooks.Hook;
 import com.greghaskins.spectrum.internal.hooks.HookContext;
 import com.greghaskins.spectrum.internal.hooks.HookContext.AppliesTo;
@@ -38,7 +37,7 @@ public final class DeclarationState {
       definitionBlock.run();
     } catch (final Throwable error) {
       suite.removeAllChildren();
-      Specification.it("encountered an error", () -> {
+      suite.addSpec("encountered an error", () -> {
         throw error;
       });
     }
